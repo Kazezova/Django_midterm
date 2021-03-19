953de57ae574336f02283f416aa840e48ae42b2f
Kazezova <anar.kazezova@gmail.com>
2021-03-20 04:01:11 +0600

    default code for mid project

---
+++ midterm/main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework import routers
+router = routers.SimpleRouter()
+# router.register('', , basename='main')
+urlpatterns = [
+]
+urlpatterns += router.urls
